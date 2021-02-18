c7b6d0e98da878f09025ce47df3f3077cd314a30
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2021-02-18 16:34:49 +0200

    added budget config

--- src/Budget/index.jsx
+++ src/Budget/index.jsx
@@ -1,12 +1,30 @@
+import React, { useState } from 'react';
+
 const Budget = () => {
-  const budget = 500;
+  const [budget, setBudget] = useState(500);
+  const [budgetVal, setBudgetVal] = useState(0);
   return(
     <div className="Budget__container App__half-container App__container_flex-columns">
       <h2>Budget:</h2>
       <div>{budget} EUR</div>
       <form className="App__container_flex-columns Add-data-form__container">
-        <input type="text" placeholder="Budget value" className="form__item" />
-        <button type="button" className="form__item">Define budget</button>
+        <input 
+          type="text" 
+          placeholder="Budget value" 
+          className="form__item" 
+          value={budgetVal}
+          onChange={(event) => { setBudgetVal(event.target.value) }}
+        />
+        <button 
+          type="button" 
+          className="form__item"
+          onClick={() => {
+            setBudget(+budgetVal);
+            setBudgetVal(0);
+          }}
+        >
+          Define budget
+        </button>
       </form>
     </div>
   );
